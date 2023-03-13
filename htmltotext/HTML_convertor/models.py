ba9f7a9c0d6d511a6976cda6e1abd7e38fda95f7
AlaaAlhoot <alaaalhoot74@gmail.com>
2023-03-13 22:23:26 +0200

    Complete

---
+++ htmltotext/HTML_convertor/models.py
@@ -0,0 +1,9 @@
+from pyexpat import model
+from django.db import models
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+
+
+class Content(models.Model):
+    text = RichTextField(blank=True, null=True)
